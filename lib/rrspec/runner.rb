c5fcc4cd7e7ffa84c813ea5a248ec395abf2cc26
obrok <pawel.obrok@gmail.com>
2011-12-09 23:57:16 +0100

    impl: load specs

--- lib/rrspec/runner.rb
+++ lib/rrspec/runner.rb
@@ -1,7 +1,14 @@
 class RRSpec::Runner
-  def run
+  def run    
     @start = Time.now
+
+    load_specs
+
     print_summary
+  end
+
+  def load_specs
+    Dir["./spec/*.rb"].each { |f| require f }    
   end
 
   def print_summary
