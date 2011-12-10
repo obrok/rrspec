ea884cc54160ff2772c0826017a3496eae94823f
obrok <pawel.obrok@gmail.com>
2011-12-11 00:04:07 +0100

    spec: should =~ fails

--- spec/expectation/should_equals_tilde_spec.rb
+++ spec/expectation/should_equals_tilde_spec.rb
@@ -2,4 +2,8 @@ describe "should =~" do
   it "should succeed" do
     ("123".should =~ /12/).should == 0
   end
+
+  it "should fail" do
+    lambda { "123".should =~ /a/ }.should raise_error
+  end
 end
