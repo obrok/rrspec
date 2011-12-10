b80d91769933da2694bf860afba4df891a816d50
obrok <pawel.obrok@gmail.com>
2011-12-10 12:36:41 +0100

    impl: SimpleFormatter prints "E"

--- lib/rrspec/formatter/simple_formatter.rb
+++ lib/rrspec/formatter/simple_formatter.rb
@@ -1,4 +1,14 @@
+require 'stringio'
+
 class RRSpec::Formatter::SimpleFormatter
+  def initialize(io=$stdout)
+    @io = io
+  end
+
   def pass
+  end
+
+  def error
+    @io.print("E")
   end
 end
