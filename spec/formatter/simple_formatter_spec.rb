209ea29990a6df734ca2408ab1b72a7d9d7d8d9f
obrok <pawel.obrok@gmail.com>
2011-12-11 00:44:11 +0100

    spec: SimpleFormatter prints exceptions

--- spec/formatter/simple_formatter_spec.rb
+++ spec/formatter/simple_formatter_spec.rb
@@ -1,13 +1,16 @@
 require 'stringio'
 formatter = RRSpec::Formatter::SimpleFormatter.new(Bootstrap::TestIO)
 
+begin; raise ArgumentError; rescue Exception => error; end
+begin; raise RRSpec::AssertionError; rescue Exception => failure; end
+
 describe RRSpec::Formatter::SimpleFormatter do
   it "prints Es on error" do
-    formatter.error
+    formatter.error(error)
   end
 
   it "prints Fs on fail" do
-    formatter.fail
+    formatter.fail(failure)
   end
 
   it "prints . on pass" do
@@ -24,11 +27,25 @@ describe RRSpec::Formatter::SimpleFormatter do
     formatter.start
     Time.fix_time(time + 19.678)
     formatter.pass
-    formatter.error
-    formatter.fail
+    formatter.error(error)
+    formatter.fail(failure)
     formatter.end
     Time.release_time
 
-    io.string.should == ".EF\n\nFinished in 19.7 seconds\n3 examples, 2 failures\n"
+    io.string.should include "\n\nFinished in 19.7 seconds\n3 examples, 2 failures\n"
+  end
+
+  it "prints errors" do
+    io = StringIO.new
+    formatter = RRSpec::Formatter::SimpleFormatter.new(io)
+
+    formatter.start
+    formatter.error(error)
+    formatter.fail(failure)
+    formatter.end
+
+    io.string.should =~ /#{error}.*#{failure}/m
+    io.string.should include error.backtrace.first
+    io.string.should include failure.backtrace.first
   end
 end
