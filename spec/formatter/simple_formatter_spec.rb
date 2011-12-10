a4765f59698b4765c7dde3405db0b8264b76f186
obrok <pawel.obrok@gmail.com>
2011-12-10 12:44:17 +0100

    spec: SimpleFormatter prints "."

--- spec/formatter/simple_formatter_spec.rb
+++ spec/formatter/simple_formatter_spec.rb
@@ -5,7 +5,12 @@ describe RRSpec::Formatter::SimpleFormatter do
     formatter.error
   end
 
-  it "prints Fs on fails" do
+  it "prints Fs on fail" do
     formatter.fail
+  end
+
+  it "prints . on pass" do
+    formatter.pass
+    Bootstrap::TestIO.string[2].should == "."
   end
 end
