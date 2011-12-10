89f238eacc9f662a4ea36713b4f86cb209f11eb3
obrok <pawel.obrok@gmail.com>
2011-12-11 00:39:58 +0100

    spec: should include fails

--- spec/expectation/should_include_spec.rb
+++ spec/expectation/should_include_spec.rb
@@ -2,4 +2,8 @@ describe "should include" do
   it "should succeed" do
     ("1234".should include "23").should == true
   end
+
+  it "should fail" do
+    lambda { "1234".should include "24" }.should raise_error
+  end
 end
