428dd5a9ff34528bf4d9c3988db28546d048ffd5
obrok <pawel.obrok@gmail.com>
2011-12-10 22:16:33 +0100

    spec: SimpleFormatter prints a summary

--- spec/formatter/simple_formatter_spec.rb
+++ spec/formatter/simple_formatter_spec.rb
@@ -1,3 +1,4 @@
+require 'stringio'
 formatter = RRSpec::Formatter::SimpleFormatter.new(Bootstrap::TestIO)
 
 describe RRSpec::Formatter::SimpleFormatter do
@@ -12,5 +13,18 @@ describe RRSpec::Formatter::SimpleFormatter do
   it "prints . on pass" do
     formatter.pass
     Bootstrap::TestIO.string[2].should == "."
+  end
+
+  it "prints a summary at the end" do
+    time = Time.now
+    io = StringIO.new
+    formatter = RRSpec::Formatter::SimpleFormatter.new(io)
+
+    Time.define_singleton_method(:now) { time }
+    formatter.start
+    Time.define_singleton_method(:now) { time + 19.678 }
+    formatter.end
+
+    io.string.should == "\n\nFinished in 19.7 seconds\n"
   end
 end
