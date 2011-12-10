07a2223466c4865d75d72aa0d43f58ba13d269e9
obrok <pawel.obrok@gmail.com>
2011-12-10 23:46:53 +0100

    spec: raise_error fails

--- spec/expectation/should_raise_error_spec.rb
+++ spec/expectation/should_raise_error_spec.rb
@@ -1,5 +1,10 @@
-describe RRSpec::Expectation do
-  it "expecting an exception should succeed" do
+describe "raise_error" do
+  it "should succeed" do
     (lambda { raise ArgumentError }.should raise_error(ArgumentError)).should == true
+  end
+
+  it "should fail" do
+    result = (lambda { }.should raise_error(ArgumentError)) rescue :raised
+    result.should == :raised
   end
 end
